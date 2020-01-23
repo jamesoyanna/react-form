0c26b3bcdf766fb969d8f36c2135f77dc3340aab
eshoiza <engrjayt200@gmail.com>
2020-01-23 16:33:19 +0100

    added userform details component

---
+++ src/components/FormUserDetail.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+
+import RaisedButton from 'material-ui/RaisedButton';
+
+export class FormUserDetail extends Component {
+    render() {
+        return <div>hello from user from details</div>;
+    }
+}
+
+export default FormUserDetail;
--- src/components/Userform.js
+++ src/components/Userform.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import FormUserDetail from './FormUserDetail';
 export class Userform extends Component {
     state = {
         step: 1,
@@ -16,33 +16,46 @@ export class Userform extends Component {
                      const { step } = this.state;
                      this.setState({
                        step: step + 1
-                     });
+                });
 
-                     //Go bavk to previous step
-                     prevtStep = () => {
-                       const { step } = this.state;
-                       this.setState({
-                         step: step - 1
-                       });
-                     };
-                 // Handle fields changes
-                 handleChange = input =>e=>{
-                     this.setState({[input]:e.target.value});
-                 }
-
-
-                   } 
-    render() {
-
-        const {step} = this.state;
-        const {firstName, lastName, email, occupation, city, bio} = this.state;
-        const values = { firstName, lastName, email, occupation, city, bio };
-        return (
-            <div>
-                
-            </div>
-        );
+    //Go bavk to previous step
+    prevtStep = () => {
+    const { step } = this.state;
+    this.setState({
+        step: step - 1
+        });
+        };
+    // Handle fields changes
+    handleChange = input =>e=>{
+        this.setState({[input]:e.target.value});
     }
+
+
+     } 
+render() {
+
+const {step} = this.state;
+const {firstName, lastName, email, occupation, city, bio} = this.state;
+const values = { firstName, lastName, email, occupation, city, bio };
+
+switch(step){
+    case 1: 
+    return (
+      <FormUserDetail
+        nextStep={this.nextStep}
+        handleChange={this.handleChange}
+        values={values}
+      />
+    )
+    case 2:
+        return <h1>FormPersonalDetails</h1>
+case 3: 
+        return <h1>Confirm</h1>
+case 4:
+    return <h1>Success</h1>     
+}
+
+}
 }
 
 export default Userform;
